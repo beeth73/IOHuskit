964e82298543ab5157f01db99349c209f22a6c09
beeth73 <181941489+beeth73@users.noreply.github.com>
2026-05-24 00:42:28 +0530

    Initial Commit

---
+++ IOHuskit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IOHuskit
+//
+//  Created by Bhushan Ladgaonkar on 24/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IOHuskit/IOHuskitApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOHuskitApp.swift
+//  IOHuskit
+//
+//  Created by Bhushan Ladgaonkar on 24/05/26.
+//
+
+import SwiftUI
+
+@main
+struct IOHuskitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
